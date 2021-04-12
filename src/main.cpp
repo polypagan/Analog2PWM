f4183dde24efda7d36d4897f43874c5a9e78b855
Daniel <48291275+polypagan@users.noreply.github.com>
2021-04-12 12:46:06 -0400

    Add files via upload
    Cleaned up.

--- src/main.cpp
+++ src/main.cpp
@@ -20,18 +20,13 @@
  */
 
 // Analog In is pin 2 (ADC3)
-const uint8_t PWM = PB4;  // pin 3
+// PWM out is pin 3
 
 void setup() {
-//  pinMode(PWM, OUTPUT);
-  DDRB |= (1 << PB4); // Set the pin PB4 as output
-//  DDRB |= (1 << PB1); // Set MISO as output for testing
-//  PORTB &= ~(1<<PB1); // And drive low.
-//  pinMode(PB1, OUTPUT);
-//  digitalWrite(PB1, LOW);
+  DDRB |= (1 << PB4); // Set the pin PB4 as output for PWM
 
 // fast PWM mode
-  GTCCR = 1<<PWM1B | 3<<COM1B0;
+  GTCCR = 1<<PWM1B | 2<<COM1B0; // was 3<<COm1b0
   TCCR1 = 3<<COM1A0 | 1<<CS10;    // was 7<<CS10;
 
   ADMUX =
@@ -45,49 +40,23 @@ void setup() {
 
   ADCSRA = 
             (1 << ADEN)  |     // Enable ADC 
+            (1 << ADATE) |
+            (1 << ADIE)  |     // FOR FREE RUN
             (1 << ADPS2) |     // set prescaler to 64, bit 2 
             (1 << ADPS1) |     // set prescaler to 64, bit 1 
             (0 << ADPS0);      // set prescaler to 64, bit 0  
 
-  ADCSRA |= (1 << ADSC);         // start first conversion          
+  ADCSRB = 0<<ADTS0;                       // Free running     
+
+  ADCSRA |= (1 << ADSC);         // start first conversion
+
+}
+
+ISR(ADC_vect) {     // for free-run
+// ADC conversion complete
+  OCR1B = ADCH;
 }
 
 void loop() {
-// read ADC, shift to 8 bits an output as PWM. 
-//  analogWrite(PWM, (analogRead(A3) >> 2));
-while (ADCSRA & (1 << ADSC)) {        // may need to wait here...
-//    PORTB = 1<<PB1;                               // raise MISO flag while wait
-//      digitalWrite(PB1, HIGH);
-    }
-//  PORTB &= ~(1<<PB1);                             // drop MISO,wait done.
-//    digitalWrite(PB1, LOW);
-
-  OCR1B = 255 - ADCH;
-  ADCSRA |= (1 << ADSC);         // start next ADC conversion
-
+// Nothing to do. Wait here while all the action happens in ISR.
 }
-/*
- * Possible refinements:
- * 
- * Run ADC in left-justified mode &
- * grab ADCH (8-bits) to pass to PWM.
- *  (saves right shift by 2): done.
- * 
- * Run ADC in timer mode (automatic acquisitions)
- * & read in each loop, or read, do PWM,
- * then start next conversion & exit loop().
- *  (hide SAR time behind execution)
- * 
- * Do read and PWM in ADCdone ISR.
- *  (ADC-driven timing)
- *  OCR1B = 255 - ADCH;
- * 
- * Do fast PWM:
- * // Configure counter/timer1 for fast PWM on PB4
- * GTCCR = 1<<PWM1B | 3<<COM1B0;
- * TCCR1 = 3<<COM1A0 | 7<<CS10;
- * then:
- * OCR1B = 255 - value;
- * done.
- * 
- */
